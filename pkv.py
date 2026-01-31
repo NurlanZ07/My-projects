655e458e1bf023f56ea0b199a6fe7404e06e04d9
Nurlan Zamanov <zamanovnurlan07@gmail.com>
2026-01-31 13:24:51 +0400

    Add files via upload

--- pkv.py
+++ pkv.py
@@ -1,9 +1,15 @@
+import json
+import sys
+
 def main():
+    notes=load_file(filename='notes.txt')
+    for key in notes.keys():
+         key=int(key)
+    note_id=max(notes.keys(), default=-1)+1
     while True:
-        notes=load_file()
         print('1. Add new note: ')
         print('2. View all notes: ')
-        print('3. View full note by ID: ')
+        print('3. View full note by id: ')
         print('4. Search notes: ')
         print('5. Delete note: ')
         print('6. Edit note: ')
@@ -21,7 +27,8 @@ def main():
 
 
         if choice==1:
-            add(notes)
+            add(note_id,notes)
+            note_id+=1
         elif choice==2:
             view_all(notes)                                           
         elif choice==3:
@@ -41,122 +48,138 @@ def main():
         else:
             print('There is no such choice.')
 
-def load_file():
-    return {}
 
-def add(notes):
-    title=input('Enter the title: ')
-    content=input('Enter the content: ')
-    tags = set(input("Enter the tags (comma separated): ").strip().lower().split(','))
-    date=input('Enter the date: ')
+def load_file(filename="notes.txt"):
+    try:
+        with open(filename, "r") as file:
+            data = json.load(file)
+            for key in data:
+                title, content, tags_list, date, difficulty = data[int(key)]
+                data[int(key)] = (title, content, set(tags_list), date, difficulty)
+            return data
+    except FileNotFoundError:
+        return {}
+    except json.JSONDecodeError:
+        return {}
+    except KeyError:
+        return{}
+
+def add(note_id,notes):
     while True:
-        if 
-        
-    difficulty=input('Entet the difficulty(between 1-5): ')
-    note=(title,content,tags,date,difficulty)
-    while True:
-        try:
-            notes[ID]=note
+        title=input('Enter the title: ')
+        if not title:
+            print('Title mustn`t be empty. ')
+        else:
             break
-        except UnboundLocalError:
-            ID=0
-    ID=+1
+    content=input('Enter the content: ')
+    tags=set(input("Enter the tags (comma separated): ").strip().lower().split(','))
+    date=input('Enter the date: ')
+    while True:   
+        try: 
+            difficulty=int(input('Entet the difficulty(between 1-5): '))
+            if 1<=difficulty<=5:
+                    break
+            else:
+                print('Difficulty must be between 1-5.  ')
+        except ValueError:
+            print('Difficulty mustn`t be empty. ')
 
+    note=(title,content,tags,date,difficulty)     
+    notes[note_id]=note
+           
 def view_all(notes):
-    for note in notes:
-        print(note)
-
+    for note_id,note in notes.items():
+        print(note_id,note)
 
 def view_by_id(notes):
     while True:
         try:
-            ask=int(input('Enter the ID: '))
+            ask=int(input('Enter the id: '))
             print(notes[ask])
             break
         except ValueError:
-            print('ID must be an integer.')
-
+            print('id must be an integer.')
 
 def search(notes):
     while True:
         try:
             ask=int(input('By which parameter do you want to search? \n 1.Tags \n 2.Diffulty \n 3. A word in title'))
             if ask==1:
-                search_word=input('Enter the tag(s): ')
-                for i in range(len(notes)):
-                    if search_word in notes.get(i,0)[2]:
-                        print(notes[i])
+                search_word=input('Enter the tag(s): ').strip().lower()
+                for key in notes:
+                    if search_word in notes.get(key,0)[2]:
+                        print(notes[key])
+                        
             elif ask==2:
-                search_word=input('Enter the difficulty: ')
-                for i in range(len(notes)):
-                    if search_word in notes.get(i,0)[4]:
-                        print(notes[i])
-            elif ask==1:
-                search_word=input('Enter the tag(s): ')
-                for i in range(len(notes)):
-                    if search_word in notes.get(i,0)[0]:
-                        print(notes[i])
-            break            
+                search_word=int(input('Enter the difficulty: '))
+                for key in notes:
+                    if search_word == notes.get(key,0)[4]:
+                        print(notes[key])
+        
+            elif ask==3:
+                search_word=input('Enter the word: ').strip()
+                for key in notes:
+                    if search_word in notes.get(key,0)[0]:
+                        print(notes[key])
+            break                    
         except ValueError:
-            print('Parameter shoulw be determined by its number.')    
+            print('Parameter should be determined by its number.')    
 
 def delete(notes):
     while True:
         try:
-            ask=int(input('Enter the ID: '))
-            del notes[ask]
-            break
+            ask=int(input('Enter the id: '))
+            if ask in notes.keys():
+                del notes[ask]
+                break
+            else:
+                print('There is no such id. ')
         except ValueError:
-            print('The ID must be an integer.')
-
+            print('The id must be an integer.')
 
 def edit(notes):
-     while True:
+    while True:
         try:
-            ask=int(input('Enter the ID: '))
+            ask=int(input('Enter the id: '))
             title=input('Enter the new title: ')
             if not title:
                 title=notes[ask][0]
             content=input('Enter the new content: ')
             if not content:
                 content=notes[ask][1]
-            tags=input('Enter the new tags: ')
+            tags=set(input('Enter the new tags: ')).strip().lower().split(',')
             if not tags:
                 tags=notes[ask][2]
             difficulty=input('Enter the new difficulty: ')
             if not difficulty:
                 difficulty=notes[ask][4]
-            notes[ask]=(title,content,tags,notes[ask][3],difficulty)   
+            notes[ask]=(title,content,tags,notes[ask][3],difficulty)    
+            break
             
         except ValueError:
-            print('The ID must be an integer.')
+            print('The id must be an integer.')
 
 def show_stats(notes):
     counts=dict()
-    tags_order=set()
-    for i in range(len(notes)):
-        for tag in notes[i][2]:
+    for key in notes.keys():
+        for tag in notes[key][2]:
             if tag in counts:
                 counts[tag]=counts.get(tag,0)+1
             else:
-               tags_order.add(tag)
-               counts[tag]=0
+               counts[tag]=1
     
-    for tag in tags_order:
-        print(f'{tag} is used in {counts.get(tag)} notes.')
+    for tag,count in counts.items():
+        print(f'{tag} is used in {count} notes.')
+    print(sorted(counts.keys()))    
 
-def save(notes):
-    with open('pkv.txt', 'w') as file:
-        file.write(notes)
+def save(notes, filename='notes.txt'):
+    notes_to_save = {k: (v[0], v[1], list(v[2]), v[3], v[4]) for k, v in notes.items()}
+    with open(filename, 'w') as file:
+        json.dump(notes_to_save, file)
+
 
 def exits():
-    return        
-
-
-
-
-
-
+    sys.exit()      
 
 main()
 
