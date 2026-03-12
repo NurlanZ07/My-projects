977377933202d822843d336fbd6d4fcd1664fc25
Nurlan Zamanov <zamanovnurlan07@gmail.com>
2026-03-12 11:43:48 +0400

    Delete nurlan.py

--- nurlan.py
+++
@@ -1,9 +0,0 @@
-def m():
-    a= int(input("type the a"))
-    b= int(input("type the b"))
-    c= int(input("type the c"))
-
-    print(sol(a,b,c))
-def sol(a,b,c):
-    return ((pow(b,2)-4*a*c)**(1/2))
-m()
